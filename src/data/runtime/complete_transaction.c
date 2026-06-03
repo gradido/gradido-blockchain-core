27f82b3e0934f879bf6878e6bfbde6ab95450b24
einhornimmond <info@einhornimmond.de>
2026-06-03 07:43:43 +0200

    make using complete_transaction easier

--- src/data/runtime/complete_transaction.c
+++ src/data/runtime/complete_transaction.c
@@ -1,4 +1,5 @@
 #include "gradido_blockchain_core/data/runtime/complete_transaction.h"
+#include "gradido_blockchain_core/data/wire/basic_types.h"
 #include "gradido_blockchain_core/memory.h"
 #include "gradido_blockchain_core/types/cross_group.h"
 #include <string.h>
@@ -14,34 +15,68 @@ void grdr_complete_transaction_release(grdr_complete_transaction *tx) {
   }
 }
 
-grd_result grdr_complete_transaction_get_sender_community_uuid(
-    uint8_t *uuid, const grdr_complete_transaction *tx
+const grdw_account_balance *grdr_complete_transaction_get_account_balance_for_public_key(
+    const grdr_complete_transaction *tx, const uint8_t public_key[32]
 ) {
-  if (!uuid || !tx) { return GRD_SUCCESS; }
-  if (GRDT_CROSS_GROUP_LOCAL == tx->cross_group_type ||
-      GRDT_CROSS_GROUP_OUTBOUND == tx->cross_group_type) {
-    memcpy(uuid, tx->tx_community_uuid, 16);
-    return GRD_SUCCESS;
-  } else if (GRDT_CROSS_GROUP_INBOUND == tx->cross_group_type) {
-    if (!tx->tx_pairing_community_uuid) { return GRD_ERROR_INVALID_STATE; }
-    memcpy(uuid, tx->tx_pairing_community_uuid, 16);
-    return GRD_SUCCESS;
+  if (!tx || !public_key) return NULL;
+  for (int i = 0; i < tx->account_balances_count; ++i) {
+    const grdw_account_balance *account_balance = &tx->account_balances[i];
+    if (0 == memcmp(account_balance->pubkey, public_key, 32)) { return account_balance; }
   }
-  return GRD_ERROR_NOT_IMPLEMENTED_YET;
+  return NULL;
 }
 
-grd_result grdr_complete_transaction_get_recipient_community_uuid(
-    uint8_t *uuid, const grdr_complete_transaction *tx
+const uint8_t *grdr_complete_transaction_get_sender_community_uuid(
+    const grdr_complete_transaction *tx
 ) {
-  if (!uuid || !tx) { return GRD_SUCCESS; }
+  if (!tx) { return NULL; }
+
+  if (GRDT_CROSS_GROUP_LOCAL == tx->cross_group_type ||
+      GRDT_CROSS_GROUP_OUTBOUND == tx->cross_group_type) {
+    return tx->tx_community_uuid;
+  } else if (GRDT_CROSS_GROUP_INBOUND == tx->cross_group_type) {
+    return tx->tx_pairing_community_uuid;
+  }
+  return NULL;
+}
+
+const uint8_t *grdr_complete_transaction_get_recipient_community_uuid(
+    const grdr_complete_transaction *tx
+) {
+  if (!tx) { return NULL; }
+
   if (GRDT_CROSS_GROUP_LOCAL == tx->cross_group_type ||
       GRDT_CROSS_GROUP_INBOUND == tx->cross_group_type) {
-    memcpy(uuid, tx->tx_community_uuid, 16);
-    return GRD_SUCCESS;
+    return tx->tx_community_uuid;
   } else if (GRDT_CROSS_GROUP_OUTBOUND == tx->cross_group_type) {
-    if (!tx->tx_pairing_community_uuid) { return GRD_ERROR_INVALID_STATE; }
-    memcpy(uuid, tx->tx_pairing_community_uuid, 16);
-    return GRD_SUCCESS;
+    return tx->tx_pairing_community_uuid;
   }
-  return GRD_ERROR_NOT_IMPLEMENTED_YET;
+  return NULL;
+}
+
+const uint8_t *grdr_complete_transaction_get_sender_public_key(
+    const grdr_complete_transaction *tx
+) {
+  if (!tx) { return NULL; }
+
+  if (GRDT_TRANSACTION_TRANSFER == tx->transaction_type ||
+      GRDT_TRANSACTION_REDEEM_DEFERRED_TRANSFER == tx->transaction_type ||
+      GRDT_TRANSACTION_DEFERRED_TRANSFER == tx->transaction_type) {
+    return tx->transfer.sender_pubkey;
+  }
+  return NULL;
+}
+
+const uint8_t *grdr_complete_transaction_get_recipient_public_key(
+    const grdr_complete_transaction *tx
+) {
+  if (!tx) { return NULL; }
+
+  if (GRDT_TRANSACTION_TRANSFER == tx->transaction_type ||
+      GRDT_TRANSACTION_REDEEM_DEFERRED_TRANSFER == tx->transaction_type ||
+      GRDT_TRANSACTION_DEFERRED_TRANSFER == tx->transaction_type ||
+      GRDT_TRANSACTION_CREATION == tx->transaction_type) {
+    return tx->transfer.recipient_pubkey;
+  }
+  return NULL;
 }
